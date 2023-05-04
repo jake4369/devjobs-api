bbfea1f6d4e15708517112fc04e914c993bbb3c8
Jake Samuels <samuels242@googlemail.com>
2023-05-04 10:58:16 +0100

    Require and use jobsRouter

--- app.js
+++ app.js
@@ -2,4 +2,12 @@ const express = require("express");
 
 const app = express();
 
+// MIDDLEWARE
+app.use(express.json());
+
+// ROUTES
+const jobsRouter = require("./routes/jobsRoutes");
+
+app.use("/api/v1/jobs", jobsRouter);
+
 module.exports = app;
