48b4b662022d26fad335df960da09eb721e76ca7
Jake Samuels <samuels242@googlemail.com>
2023-05-04 10:58:37 +0100

    Set up test suite

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,3 +1,21 @@
+const { default: mongoose } = require("mongoose");
 const request = require("supertest");
 const app = require("../app");
 const data = require("../data/dev-data/data.json");
+
+require("dotenv").config({
+  path: "../config.env",
+});
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+beforeEach(async () => {
+  await mongoose.connect(DB);
+});
+
+afterEach(async () => {
+  await mongoose.connection.close();
+});
