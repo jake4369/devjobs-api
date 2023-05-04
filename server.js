e36c39221f5a3e6465b83ed0b72bdb629c46699f
Jake Samuels <samuels242@googlemail.com>
2023-05-04 10:43:04 +0100

    Connect to DB

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
+const mongoose = require("mongoose");
+
 const app = require("./app");
+
+require("dotenv").config({
+  path: "./config.env",
+});
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connection successful 🎉"));
 
 const PORT = process.env.PORT || 3000;
 
