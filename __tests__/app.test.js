27680f3796838ac6be1669d3b597c97861b4889a
Jake Samuels <samuels242@googlemail.com>
2023-05-04 11:27:03 +0100

    Write test for GET /api/v1/jobs

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,7 +4,7 @@ const app = require("../app");
 const data = require("../data/dev-data/data.json");
 
 require("dotenv").config({
-  path: "../config.env",
+  path: "./config.env",
 });
 
 const DB = process.env.DATABASE.replace(
@@ -18,4 +18,30 @@ beforeEach(async () => {
 
 afterEach(async () => {
   await mongoose.connection.close();
+});
+
+describe("GET /api/v1/jobs", () => {
+  it("should return an array of all jobs", async () => {
+    const expectedObject = {
+      company: expect.any(String),
+      logo: expect.any(String),
+      logoBackground: expect.any(String),
+      position: expect.any(String),
+      postedAt: expect.any(String),
+      contract: expect.any(String),
+      location: expect.any(String),
+      website: expect.any(String),
+      apply: expect.any(String),
+      description: expect.any(String),
+      requirements: expect.any(Object),
+      role: expect.any(Object),
+    };
+
+    const res = await request(app).get("/api/v1/jobs");
+    const { jobs } = res.body.data;
+    expect(res.statusCode).toBe(200);
+    expect(Array.isArray(jobs)).toBe(true);
+    expect(jobs.length).toBe(15);
+    jobs.forEach((job) => expect(job).toMatchObject(expectedObject));
+  });
 });
