a18e8c794e29baef7c67b43e1728a89f9920a65e
Jake Samuels <samuels242@googlemail.com>
2023-05-05 10:23:57 +0100

    Filter and paginate jobs

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,9 +1,18 @@
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 const Jobs = require("../models/jobsModel");
+const APIFeatures = require("../utils/apiFeatures");
 
 exports.getAllJobs = catchAsync(async (req, res, next) => {
-  const jobs = await Jobs.find();
+  // 1) Keyword search
+  const searchTerm = req.query.search;
+  if (searchTerm) {
+    req.query.position = { $regex: searchTerm, $options: "i" };
+  }
+
+  const features = new APIFeatures(Jobs.find(), req.query).filter().paginate();
+
+  const jobs = await features.query;
 
   res.status(200).json({
     status: "success",
