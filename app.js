5cf505dca6876a856346d06340d4ee261f6d7aa5
Jake Samuels <samuels242@googlemail.com>
2023-07-17 17:22:48 +0100

    Set security HTTP headers with helmet

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 
@@ -9,6 +10,10 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 // GLOBAL
+// Set security HTTP header
+app.use(helmet());
+
+// Limit requests from the same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -18,7 +23,12 @@ const limiter = rateLimit({
 app.use("/api", limiter);
 
 // MIDDLEWARE
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(
+  express.json({
+    limit: "10kb",
+  })
+);
 
 // ROUTES
 app.use("/api/v1/jobs", jobsRouter);
