d866de7f908225e8482169855e797c07d4d202cc
Jake Samuels <samuels242@googlemail.com>
2023-07-17 17:30:58 +0100

    Sanitize data against NoSQL query injection and cross-site scripting attacks

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 
@@ -10,7 +12,7 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 // GLOBAL
-// Set security HTTP header
+// Set security HTTP headers
 app.use(helmet());
 
 // Limit requests from the same IP
@@ -29,6 +31,12 @@ app.use(
     limit: "10kb",
   })
 );
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // ROUTES
 app.use("/api/v1/jobs", jobsRouter);
