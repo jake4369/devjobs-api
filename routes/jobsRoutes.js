43dcf2b189217473100c92e18c288ef4e4cd2d0e
Jake Samuels <samuels242@googlemail.com>
2023-07-17 10:46:04 +0100

    GET, POST and DELETE job routes

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const jobsController = require("../controllers/jobsController");
+const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-router.route("/").get(jobsController.getAllJobs);
+router
+  .route("/")
+  .get(authController.protect, jobsController.getAllJobs)
+  .post(jobsController.createJob);
+
+router
+  .route("/:id")
+  .get(jobsController.getJob)
+  .delete(
+    authController.protect,
+    authController.restrictTo("admin", "employer"),
+    jobsController.deleteJob
+  );
 
 module.exports = router;
