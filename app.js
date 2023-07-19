08118a02adaf489568fbd45b479ff43d82601436
Jake Samuels <samuels242@googlemail.com>
2023-07-19 10:33:42 +0100

    Install cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
@@ -11,6 +12,8 @@ const jobsRouter = require("./routes/jobsRoutes");
 const userRouter = require("./routes/userRoutes");
 
 const app = express();
+
+app.use(cors());
 
 // GLOBAL
 // Set security HTTP headers
