2694d5bc426369863ed93da2f2ae0cadd2b43bf8
Jake Samuels <samuels242@googlemail.com>
2023-07-21 11:58:35 +0100

    Increase API call rate

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(helmet());
 
 // Limit requests from the same IP
 const limiter = rateLimit({
-  max: 100,
+  max: 1000,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP, please try again in 1 hour!",
 });
